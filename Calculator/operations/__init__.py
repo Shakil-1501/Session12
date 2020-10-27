41a6e078b2362c253f5da846d87db213ad2bae3e
Shakil-1501 <63920152+Shakil-1501@users.noreply.github.com>
2020-10-27 22:32:36 +0530

    Add files via upload

---
+++ Calculator/operations/__init__.py
@@ -0,0 +1,13 @@
+from .sine import *
+from .cose import *
+from .tane import *
+from .tanhe import *
+from .ee import *
+from .loge import *
+from .sigmoide import *
+from .relue import *
+from .softmaxe import *
+
+__all__= (sine.__all__+ cose.__all__ + tane.__all__ + tanhe.__all__ + ee.__all__ + loge.__all__ + sigmoide.__all__ + relue.__all__ + softmaxe.__all__)
+
+
